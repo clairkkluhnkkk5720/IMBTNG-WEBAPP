53b0463f4ce0961cf57f3f94b3916208f40d8a37
Sagir Hossain <sagir.hossain.524@gmail.com>
2018-02-22 15:15:47 +0600

    Creating Admin auth functions

--- app/Http/Controllers/Dashboard/AuthController.php
+++ app/Http/Controllers/Dashboard/AuthController.php
@@ -12,4 +12,29 @@ class AuthController extends Controller
     {
     	return view('dashboard.auth.login');
     }
+
+    public function showResetLinkRequestPage ()
+    {
+    	return view('dashboard.auth.password.request');
+    }
+
+    public function showPasswordResetPage ($token)
+    {
+    	return view('dashboard.auth.password.reset');
+    }
+
+    public function sentResetLinkEmail (Request $request)
+    {
+    	// Send email & redirect to password reset page
+    }
+
+    public function resetPassword (Request $request)
+    {
+    	// reset user password
+    }
+
+    public function logout ()
+    {
+    	// logs out user
+    }
 }
