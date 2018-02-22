c0a07fb62681e76391014e22f98b3e120946c8c8
Sagir Hossain <sagir.hossain.524@gmail.com>
2018-02-22 20:54:36 +0600

    Configuring guest middleware for admins

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,20 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        $redirectUri = '/';
+
+        switch ($guard) {
+            case 'admin':
+                if (Auth::guard($guard)->check()) {
+                    return redirect()->route('dashboard.index');
+                }
+                break;
+
+            default :
+                if (Auth::guard($guard)->check()) {
+                    return redirect('/home');
+                }
+                break;
         }
 
         return $next($request);
--- resources/views/dashboard/layouts/auth.blade.php
+++ resources/views/dashboard/layouts/auth.blade.php
@@ -29,7 +29,7 @@
 <!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition register-page">
+<body class="hold-transition login-page">
 
 <div class="login-box">
 	<div class="login-logo">
