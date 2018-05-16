4c94b1e942eaddfadc63bdd6b8cc1fc60021c93d
Eugene Hatsko <e.chatzko@wis.eu.com>
2018-05-16 16:49:12 +0300

    Admin class was implemented for User model (change password, separated tabs, etc)

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from apps.users.models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone',
+                                         'avatar')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
